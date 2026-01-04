20163d7d302bdb1721cbd66b4b4af80960fbd946
Alex Lelievre <alex@folabs.com>
2026-01-03 20:16:27 -0800

    Changed brightness to happen after blend

--- led-server.ino
+++ led-server.ino
@@ -414,13 +414,31 @@ void fade_up_rgb( int r, int g, int b, int steps, useconds_t latency )
 // mu = 0..1 (0 = rgb1 and 1 = rgb2, anywhere between is a blend)
 void blend_rgb( int r1, int g1, int b1, int r2, int g2, int b2, float mu )
 {
+    int brightnessInverse = 255 - sSettings.brightness;
+  
     if( mu < 0.0f )
         mu = 0.0f;
     if( mu > 1.0f )
         mu = 1.0f;
 
     float oneMinusAlpha = 1.0f - mu;
-    set_rgb( r2 * mu + (r1 * oneMinusAlpha), g2 * mu + (g1 * oneMinusAlpha), b2 * mu + (b1 * oneMinusAlpha) );    
+    int r = r2 * mu + (r1 * oneMinusAlpha);
+    int g = g2 * mu + (g1 * oneMinusAlpha);
+    int b = b2 * mu + (b1 * oneMinusAlpha);
+
+    int rb = r - brightnessInverse;
+    if( rb < 0 )
+      rb = 0;
+      
+    int gb = g - brightnessInverse;
+    if( gb < 0 )
+      gb = 0;
+
+    int bb = b - brightnessInverse;
+    if( bb < 0 )
+      bb = 0;
+    
+    set_rgb( rb, gb, bb );    
 }
 
 
@@ -436,15 +454,13 @@ void fade_to_rgb( int r, int g, int b, int from_r, int from_g, int from_b, int s
 
 bool fade_to_rgb_with_state( int r, int g, int b, int from_r, int from_g, int from_b, int steps, useconds_t latency )
 {
-  int brightnessInverse = 255 - sSettings.brightness;
-   
   if( sFade_step >= steps )
   {
     sFade_step = 0;
     return true;
   }
     
-  blend_rgb( r - brightnessInverse, g - brightnessInverse, b - brightnessInverse, from_r - brightnessInverse, from_g - brightnessInverse, from_b - brightnessInverse, sFade_step * (1.0f / steps) );
+  blend_rgb( r, g, b, from_r, from_g, from_b, sFade_step * (1.0f / steps) );
   delayMicroseconds( latency );  
   ++sFade_step;
   return false; // not done
